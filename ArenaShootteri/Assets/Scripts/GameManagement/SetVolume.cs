844a6fcf041b46b40d1ccbb2c3c6c0cb63e570a4
Dragonath <dragonathplays@gmail.com>
2020-10-10 18:12:05 +0300

    MainMenu loppuun, Sound/Music manager + options sliders

---
+++ ArenaShootteri/Assets/Scripts/GameManagement/SetVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixerGroup targetGroup;
+    public SoundManager soundManager;
+    public AudioMixer mixer;
+    public Slider slider;
+
+    private void Start()
+    {
+        soundManager = FindObjectOfType<SoundManager>();
+        slider.value = PlayerPrefs.GetFloat(targetGroup.name);
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        soundManager.SetVolume(targetGroup, sliderValue);
+    }
+}
