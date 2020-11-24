0b5c2235cba5fe736357d3b19c5e837241fe9183
Dragonath <dragonathplays@gmail.com>
2020-11-24 13:53:10 +0200

    controls

--- ArenaShootteri/Assets/Scripts/GameManagement/MainMenu.cs
+++ ArenaShootteri/Assets/Scripts/GameManagement/MainMenu.cs
@@ -10,6 +10,8 @@ public class MainMenu : MonoBehaviour
     public GameObject optionsPanel;
     public GameObject creditsPanel;
     public GameObject exitPanel;
+    public GameObject controlsPanel;
+    public GameObject highscorePanel;
     public GameObject timeline;
 
     // Start is called before the first frame update
@@ -29,7 +31,7 @@ public class MainMenu : MonoBehaviour
         timeline.SetActive(false);
         SceneManager.LoadScene(1);
     }
-
+    //Settings buttons
     public void OpenSettings()
     {
         mainPanel.SetActive(false);
@@ -40,7 +42,7 @@ public class MainMenu : MonoBehaviour
         mainPanel.SetActive(true);
         optionsPanel.SetActive(false);
     }
-
+    //Credits buttons
     public void OpenCredits()
     {
         mainPanel.SetActive(false);
@@ -51,7 +53,29 @@ public class MainMenu : MonoBehaviour
         mainPanel.SetActive(true);
         creditsPanel.SetActive(false);
     }
-
+    //Controls buttons
+    public void OpenControls()
+    {
+        mainPanel.SetActive(false);
+        controlsPanel.SetActive(true);
+    }
+    public void CloseControls()
+    {
+        mainPanel.SetActive(true);
+        controlsPanel.SetActive(false);
+    }
+    //Highscore buttons
+    public void OpenHighScore()
+    {
+        mainPanel.SetActive(false);
+        highscorePanel.SetActive(true);
+    }
+    public void CloseHighScore()
+    {
+        mainPanel.SetActive(true);
+        highscorePanel.SetActive(false);
+    }
+    //QuitGame buttons
     public void OpenExit()
     {
         mainPanel.SetActive(false);
