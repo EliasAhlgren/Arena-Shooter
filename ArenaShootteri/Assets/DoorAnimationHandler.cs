5f3b7cc5decf9215ebb78000548ca2cd56a63f55
Aku Telimaa <43701576+Wabbis@users.noreply.github.com>
2020-11-29 17:35:16 +0200

    PyöröOvi + PacMan(start) / Removed some old stuff

---
+++ ArenaShootteri/Assets/DoorAnimationHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimationHandler : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.transform.CompareTag("Player"))
+        {
+            anim.Play("OvenAvaus");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.transform.CompareTag("Player"))
+        {
+            anim.Play("OvenSulku");
+        }
+    }
+}
---
+++ ArenaShootteri/Assets/PacManHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacManHandler : MonoBehaviour
+{
+    public bool empty = true;
+    public int count;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        count++;
+        if(count > 0)
+        {
+            empty = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        count--;
+        if(count == 0)
+        {
+            empty = true;
+        }
+    }
+
+
+
+}
