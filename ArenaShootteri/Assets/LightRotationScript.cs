bbfb32857da50cfe7eaaf16fc9385d77be557279
Aku Telimaa <43701576+Wabbis@users.noreply.github.com>
2020-12-03 16:57:57 +0200

    OviLamppu

---
+++ ArenaShootteri/Assets/LightRotationScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotationScript : MonoBehaviour
+{
+    public float step = 2;
+    void Update()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+        transform.Rotate(new Vector3(step, 0,0), Space.World);
+    }
+}
